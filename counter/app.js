5babebae678d1c703dddaf3aaf91b7d75c42d14a
MitulPatil <patilmitul1911@gmail.com>
2024-06-06 19:06:22 +0530

    increament & decreament counter

--- counter/app.js
+++ counter/app.js
@@ -4,6 +4,7 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
